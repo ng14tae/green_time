254c8c400cba212638f6fcd60e08ff87be293dc1
Tae JeongHwa <eh@TaenoMacBook-Air.local>
2025-09-29 12:41:21 +0900

    add:💡0929-gem devise実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   # バリデーション
   validates :line_user_id, presence: true, uniqueness: true
   validates :display_name, length: { maximum: 255 }
