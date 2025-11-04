431b2d334b79c059ac278252c2f49feebc6cb6e6
Tae JeongHwa <eh@TaenoMacBook-Air.local>
2025-11-04 23:15:05 +0900

    fix:🐟1104-db修正

--- db/migrate/20251104053957_add_line_fields_to_users.rb
+++ db/migrate/20251104053957_add_line_fields_to_users.rb
@@ -1,6 +1,5 @@
 class AddLineFieldsToUsers < ActiveRecord::Migration[7.2]
   def change
-    add_column :users, :line_user_id, :string
     add_column :users, :avatar_url, :string
     add_column :users, :nickname, :string
   end
