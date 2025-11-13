cae475d896d7d9f352e5f1a0c6995e248428cc28
Tae JeongHwa <eh@TaenoMacBook-Air.local>
2025-11-13 15:49:58 +0900

    design:🎨1113-あれおかしいな

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,7 +53,7 @@ Rails.application.configure do
     .tap  { |logger| logger.formatter = ::Logger::Formatter.new }
     .then { |logger| ActiveSupport::TaggedLogging.new(logger) }
 
-  config.log_tags = [:request_id]
+  config.log_tags = [ :request_id ]
   config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
 
   # Caching for mailer templates
