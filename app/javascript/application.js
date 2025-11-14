2d5612683ca8ca2faf10ced2cc1696d7f8af4e2c
Tae JeongHwa <eh@TaenoMacBook-Air.local>
2025-11-14 18:11:14 +0900

    fix:📈1114-さぁて

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,6 +4,8 @@ import "./controllers"
 import "./current_time"
 import "chart.js/auto"
 import "chartkick/chart.js"
+import { drawMoodLineChart } from "./charts/mood_line_chart.js";
+window.drawMoodLineChart = drawMoodLineChart;
 
 document.addEventListener("turbo:load", () => {
     Chartkick.eachChart((chart) => chart.redraw());
