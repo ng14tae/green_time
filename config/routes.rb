00c74cad726072d1656c098f63089969a93a0284
Tae JeongHwa <eh@TaenoMacBook-Air.local>
2025-11-18 23:12:43 +0900

    re:🌀1118-ミス！

--- config/routes.rb
+++ config/routes.rb
@@ -29,9 +29,9 @@ Rails.application.routes.draw do
 
   resources :checkinout_records, only: [] do
     collection do
-      get "checkin", to: "checkinout_records#checkin_page", as: :checkin_page
+      get "checkin", to: "checkinout_records#checkin_page", as: :checkin
       post "checkin", to: "checkinout_records#checkin"
-      get "checkout", to: "checkinout_records#checkout_page", as: :checkout_page
+      get "checkout", to: "checkinout_records#checkout_page", as: :checkout
       patch "checkout", to: "checkinout_records#checkout"
     end
   end
